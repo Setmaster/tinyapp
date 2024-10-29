c69cdcba7daa7fe4ff32c090319fb120bae3860a
Vitor <vi7org@hotmail.com>
2024-10-29 13:38:55 -0400

    display username and hide form if user is logged in

--- express_server.js
+++ express_server.js
@@ -1,11 +1,12 @@
 ﻿const {generateRandomString} = require("./utilFunctions")
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
-
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); // urlencoded will convert the request body from a Buffer into string that we can read. It will then add the data to the req(request) object under the key body
+app.set("view engine", "ejs");
 
 const urlDatabase = {
     b2xVn2: "http://www.lighthouselabs.ca",
@@ -21,16 +22,26 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
+    const templateVars = {
+        urls: urlDatabase,
+        username: req.cookies["username"],
+    };
     res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+    const templateVars = {
+        username: req.cookies["username"],
+    };
+    res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+    const templateVars = {
+        id: req.params.id,
+        longURL: urlDatabase[req.params.id],
+        username: req.cookies["username"],
+    };
     res.render("urls_show", templateVars);
 });
 
